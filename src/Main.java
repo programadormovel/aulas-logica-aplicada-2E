d9abfcca545999f105a2a77775f11deea54f1397
programadormovel <programadormovel@gmail.com>
2023-05-04 11:54:59 -0300

    Aula de introdução OO

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        // laço enquanto - while
-        int contador = 0;
-        while(contador < 10){
-            System.out.println("Contador = " + contador);
-            // incremento
-            contador = contador + 1;
-        }
-        // laço do para faça - for
-        for(int x = 0; x < 10; x++){
-            System.out.println("Contador = " + x);
-        }
-        // laço faça enquanto - do while
-        int cont = 1;
-        do {
-            // processo
-            System.out.println(cont);
-            cont++; //incremento
-        }while(cont <= 10);
+        // Aula introdutória de Orientação à Objetos
+
     }
 }
